7adf994e827adb839e61864394b3ea718efcf6cd
Julie Altman <jtskolnik@gmail.com>
2020-12-25 12:24:00 -0500

    changed calculator inputs

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
-$(function(){
-console.log("test");
+$(function () {
+    const acidInputEl = $("#acidName");
+
+    //Display what the user enters into the calculator
+    $(document).on("click", ".calc-button", function(event){
+        event.preventDefault();
+
+        console.log("test");
+    })
+
+    $(document).on("click", ".name-submit", function (event) {
+        event.preventDefault();
+
+        let acidName = acidInputEl.val().trim();
+
+        console.log("test2");
+        
+    }); //Closes event listener for submitting the acid name 
 });//Closes jquery function

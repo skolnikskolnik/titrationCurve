484b3f5003c4a771204639ab5c69b1b51c4d4ccd
Julie Altman <jtskolnik@gmail.com>
2020-12-26 13:12:14 -0500

    framework for input validation set up

--- public/js/index.js
+++ public/js/index.js
@@ -32,9 +32,13 @@ $(function () {
         //pKaorKa=1 for pKa and it equals 2 for Ka
         let pKaorKa = $(".pKaorKa").val();
 
+
         //pKa can only have two decimal places and Ka can only have 6 decimal places
         let pKa;
         let regKa;
+
+        //We need to validate the inputs before proceeding 
+        //The acid name must be a string - we only need to check it it is blank or not
 
         if (pKaorKa == 1) {
             //Need to take number and make it pKa
@@ -77,7 +81,7 @@ $(function () {
             }
 
         }
-        
+
 
         //Reset number string to zero/blank
         numberString = "";
@@ -86,31 +90,42 @@ $(function () {
 
 
     //function to store acid in db
-    function addAcidToDB(pKa, regKa){
+    function addAcidToDB(pKa, regKa) {
         let acidName = acidInputEl.val().trim();
 
-        let acidInput = {
-            acid_name: acidName,
-            pKa: pKa,
-            Ka: regKa
-        }
+        if (acidName.length == 0) {
+            console.log("You must enter a valid name!");
+        } else {
+            //pKa and Ka must both be numbers
+            if (isNaN(pKa) || isNaN(regKa)) {
+                console.log("You must enter a valid pKa or Ka!");
+            }
+            else {
+                let acidInput = {
+                    acid_name: acidName,
+                    pKa: pKa,
+                    Ka: regKa
+                }
 
-        $.ajax(`/newacid/${acidName}/${pKa}/${regKa}`, {
-            type: "POST", 
-            data: JSON.stringify(acidInput),
-            dataType: 'json',
-            contentType: 'application/json'
-        }).then(function(){
-            console.log("test");
-            location.reload();
-        });
+                $.ajax(`/newacid/${acidName}/${pKa}/${regKa}`, {
+                    type: "POST",
+                    data: JSON.stringify(acidInput),
+                    dataType: 'json',
+                    contentType: 'application/json'
+                }).then(function () {
+                    //If not found we need to warn the user that the value was no good
+                    location.reload();
+                });
+            }//Ends else statement for validating that pKa and/or Ka are numbers
+        }//Ends the else statement to validate that a name has been added
     }
+
+
 
     //Function definition for base 10
     function log10(val) {
         return Math.log(val) / Math.LN10;
     }
 
-    //Function to see if a string 
 
 });//Closes jquery function
